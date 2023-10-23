454e4238cb27751cfadca3d51d788436494e09fa
Tornelius Broadwater jr <tkbjr01@icloud.com>
2023-10-23 16:10:59 -0400

    Added UI and improved flow

--- js/script.js
+++ js/script.js
@@ -1,27 +1,58 @@
 
-playGame();
+// playGame();
 
-function playGame() {
-  const inputMessage = "Please choose either Rock, Paper, or Scissors";
-  let score = 0;
-  let roundNumber = 1;
+// function playGame() {
+//   const inputMessage = "Please choose either Rock, Paper, or Scissors";
+//   let score = 0;
+//   let roundNumber = 1;
 
-  while (roundNumber <= 5) {
-    const rawUserSelection = prompt(`
-      roundNumber: ${roundNumber}
-      ${inputMessage}
-    `);
-    const userSelection = rawUserSelection.toLowerCase();
+//   while (roundNumber <= 5) {
+//     const rawUserSelection = prompt(`
+//       roundNumber: ${roundNumber}
+//       ${inputMessage}
+//     `);
+//     const userSelection = rawUserSelection.toLowerCase();
 
-    const computerSelection = getComputerChoice();
-    const result = playRound(userSelection, computerSelection);
+//     const computerSelection = getComputerChoice();
+//     const result = playRound(userSelection, computerSelection);
 
-    alert(result)
-    roundNumber += 1
-  }
+//     alert(result)
+//     roundNumber += 1
+//   }
+// }
+
+let selectionButns = document.querySelectorAll('.selection-button');
+let gameResultText = document.querySelector('.game-result');
+let scoreText = document.querySelector('.score');
+let detailText = document.querySelector('.detail');
+
+let win = 0;
+let loss = 0;
+
+for(let i = 0; i < selectionButns.length; i++) {
+  let butn = selectionButns[i];
+  butn.addEventListener('click', (e) => {
+    const computerChoice = getComputerChoice();
+    detailText.textContent = playRound(e.target.textContent.toLowerCase(), computerChoice);
+
+    scoreText.textContent = `Win: ${win} - loss ${loss}`;
+
+    if (win + loss === 5) {
+      if (win > loss) {
+        gameResultText.textContent = `Game Over: You Win`;
+      } else {
+        gameResultText.textContent = `Game Over: You Lose`;
+      }
+      win = 0;
+      loss = 0;
+    } else {
+      gameResultText.textContent = ''
+    }
+
+  });
 }
 
-// Create a function for the computer to generate a random choice
+// // Create a function for the computer to generate a random choice
 function getComputerChoice() {
   const randomNumber = Math.random();
   if (randomNumber <= (1/3)) {
@@ -35,11 +66,16 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock") {
+    //console.log(handleRockSelection(computerSelection));
     return handleRockSelection(computerSelection);
-  } else if (playerSelection === " paper") {
+  } else if (playerSelection === "paper") {
+    //console.log(handlePaperSelection(computerSelection));
     return handlePaperSelection(computerSelection);
-  } else {
+  } else if (playerSelection === "scissors") {
+    //console.log(handleScissorsSelection(computerSelection));
     return handleScissorsSelection(computerSelection);
+  } else {
+    return;
   }
 }
 
@@ -48,8 +84,10 @@ function handleRockSelection(cpu) {
   if (cpu === "rock") {
     return "You Tie, both selected rock!";
   } else if (cpu === "paper") {
+    loss++;
     return "You Lose, paper beats rock";
   } else {
+    win++;
     return "You Win, rock beast scissors";
   }
 }
@@ -57,10 +95,12 @@ function handleRockSelection(cpu) {
 // handles when user selects paper
 function handlePaperSelection(cpu) {
   if (cpu === "rock") {
+    win++;
     return "You Win, paper beats rock";
   } else if (cpu === "paper") {
     return "You Tie, both selected paper!";
   } else {
+    loss++;
     return "You Lose, scissors beast paper";
   }
 }
@@ -68,8 +108,10 @@ function handlePaperSelection(cpu) {
 // handles when user selects scissors
 function handleScissorsSelection(cpu) {
   if (cpu === "rock") {
+    loss++;
     return "You Lose, rock beats scissors";
   } else if (cpu === "paper") {
+    win++;
     return "You Win, scissors beats paper";
   } else {
     return "You Tie, both selected scissors!";
